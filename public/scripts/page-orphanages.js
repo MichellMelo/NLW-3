fe9c2b436336545ece7aa10a1805a1bfeb9ebaf7
MichellMelo <michellmelo58@gmail.com>
2020-10-30 19:22:08 -0300

    Page orphanage

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -5,9 +5,22 @@ const map = L.map('mapid').setView([-3.7796419, -38.5571208], 15);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 // create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
 
+// create popup overlay
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar Batista <a href="orphanage.html?id=1 "> <img src="./public/images/arrow-white.svg"> </a>')
 
-L.marker([-3.7796419, -38.5571208])
+// create and add marker
+L.marker([-3.7796419, -38.5571208], { icon })
     .addTo(map)
-    .bindPopup('Lar Batista')
-    .openPopup();
+    .bindPopup(popup)
